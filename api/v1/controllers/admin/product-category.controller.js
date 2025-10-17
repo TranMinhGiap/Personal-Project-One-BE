9bce56f05327f544084036e15a88246bcef02101
Tran Minh Giap <tranthanhhuyen0404@gmail.com>
2025-10-17 16:37:23 +0700

    Phan trang danh muc san pham ben admin

--- api/v1/controllers/admin/product-category.controller.js
+++ api/v1/controllers/admin/product-category.controller.js
@@ -1,5 +1,6 @@
 const ProductCategory = require('../../models/product-category.modal');
 const sendErrorHelper = require('../../../../helpers/sendError.helper');
+const paginationHelper = require("../../../../helpers/objectPagination.helper");
 
 // [GET] /api/v1/product-category
 module.exports.index = async (req, res) => {
@@ -7,12 +8,18 @@ module.exports.index = async (req, res) => {
     const condition = {
       deleted: false
     }
-    const records = await ProductCategory.find(condition);
+    // Pagination
+    const countDocument = await ProductCategory.countDocuments(condition);
+    const objectPagination = paginationHelper.objectPagination(req.query, countDocument)
+    const records = await ProductCategory.find(condition)
+      .skip(objectPagination.skip)
+      .limit(objectPagination.limit);
     res.json({
       success: true,
       status: 200,
       message: "Lấy dữ liệu thành công !",
       data: records,
+      pagination: objectPagination
     });
   } catch (error) {
     sendErrorHelper.sendError(res, 500, "Lỗi server", error.message);
---
+++ helpers/objectPagination.helper.js
@@ -0,0 +1,21 @@
+module.exports.objectPagination = (query, totalRecord) => {
+  const defaultLimit = 15;
+  let limit = parseInt(query.limit) || defaultLimit;
+  limit = Math.max(1, Math.min(limit, 100)); // giới hạn 1–100
+
+  const totalPage = Math.ceil(totalRecord / limit) || 1;
+
+  let currPage = parseInt(query.page) || 1;
+  currPage = Math.max(1, Math.min(currPage, totalPage));
+
+  const skip = (currPage - 1) * limit;
+
+  return {
+    currPage,
+    limit,
+    skip,
+    totalPage,
+    page_next: currPage < totalPage ? currPage + 1 : null,
+    page_prev: currPage > 1 ? currPage - 1 : null
+  };
+};
