0717a7f6480b30fdc0e6de33246f2c9f00bc6a41
Tran Minh Giap <tranthanhhuyen0404@gmail.com>
2025-11-01 12:18:45 +0700

    Them thong tin role cho myAccount

--- api/v1/controllers/admin/my-account.controller.js
+++ api/v1/controllers/admin/my-account.controller.js
@@ -1,6 +1,20 @@
 const Account = require('../../models/account.model');
+const Role = require('../../models/role.model');
+const sendErrorHelper = require('../../../../helpers/sendError.helper');
 
 module.exports.index = async (req, res) => {
+  // Gan thông tin role
+  if (req.user.role_id) {
+    try {
+      const role = await Role.findOne({ _id: req.user.role_id, status: "active", deleted: false }).lean().select("title");
+      req.user = {
+        ...req.user.toObject?.() || req.user,
+        roleInfo: role || null
+      };
+    } catch (error) {
+      return sendErrorHelper.sendError(res, 500, "Không lấy được thông tin phân quyền", error.message);
+    }
+  }
   res.json({
     success: true,
     status: 200,
