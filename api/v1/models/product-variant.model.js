b28833123e9b4f08151f60d74a0b3afa3c0c2ec8
Tran Minh Giap <tranthanhhuyen0404@gmail.com>
2025-11-08 12:10:16 +0700

    Tao model product + product-variant

---
+++ api/v1/models/product-variant.model.js
@@ -0,0 +1,81 @@
+const mongoose = require("mongoose");
+const slug = require('mongoose-slug-updater');
+mongoose.plugin(slug);
+
+const productVariantSchema = new mongoose.Schema(
+  { 
+    product_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",  
+      required: true
+    },
+    price: { 
+      type: Number, 
+      required: true,
+      default: 0
+    },
+    discountPercentage: { 
+      type: Number, 
+      default: 0
+    },
+    stock: { 
+      type: Number,
+      required: true,
+      default: 0 
+    },
+    storage: { 
+      type: String, 
+      required: true
+    },
+    color: { 
+      type: String,
+      required: true
+    },
+    createdBy: {
+      account_id: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Account" 
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now
+      }
+    },
+    images: {
+      type: [String],
+      default: []
+    },
+    status: {
+      type: String,
+      enum: ['active', 'inactive'], 
+      default: 'active'
+    },
+    deleted: {
+      type: Boolean,
+      default: false
+    },
+    deletedBy: {
+      account_id: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Account" 
+      },
+      deletedAt: Date
+    },
+    updatedBy: [
+      {
+        account_id: { 
+          type: mongoose.Schema.Types.ObjectId, 
+          ref: "Account" 
+        },
+        updatedAt: Date
+      }
+    ],
+    slug: { type: String, slug: "color", unique: true }
+  }, {
+    timestamps: true
+  }
+);
+
+const ProductVariant = mongoose.model('ProductVariant', productVariantSchema, "products-variant");
+
+module.exports = ProductVariant;
---
+++ api/v1/models/product.model.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+const slug = require('mongoose-slug-updater');
+mongoose.plugin(slug);
+
+const productSchema = new mongoose.Schema(
+  { 
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    category_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ProductCategory",
+      required: true
+    },
+    createdBy: {
+      account_id: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Account" 
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now
+      }
+    },
+    description: {
+      type: String,
+      default: ""
+    },
+    detail: {
+      type: String,
+      default: ""
+    },
+    thumbnail: {
+      type: [String],
+      default: []
+    },
+    status: {
+      type: String,
+      enum: ['active', 'inactive'], 
+      default: 'active'
+    },
+    position: Number,
+    deleted: {
+      type: Boolean,
+      default: false
+    },
+    deletedBy: {
+      account_id: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Account" 
+      },
+      deletedAt: Date
+    },
+    updatedBy: [
+      {
+        account_id: { 
+          type: mongoose.Schema.Types.ObjectId, 
+          ref: "Account" 
+        },
+        updatedAt: Date
+      }
+    ],
+    slug: { type: String, slug: "title", unique: true }
+  }, {
+    timestamps: true
+  }
+);
+
+const Product = mongoose.model('Product', productSchema, "products");
+
+module.exports = Product;
